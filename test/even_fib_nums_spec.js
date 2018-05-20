1eb4392a8a25b908f9179a3b2d017303eb2a6f78
Den Drotar <naruto_1218@hotmail.com>
2018-05-20 10:10:39 -0700

    show sum of even fibo numbers when run npm test in the console

--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -7,15 +7,18 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
   it( 'should return the sum of all even Fibonacci numbers when given 13', function() {
     var sum = fibonnaciNums.sumFibs( 13, 1, 2 );
     sum.should.equal( 10 );
+    console.log('Sum = ', sum);
   });
 
   it( 'should return the sum of all even Fibonacci numbers when given 34', function() {
     var sum = fibonnaciNums.sumFibs( 34, 1, 2 );
     sum.should.equal( 44 );
+    console.log('Sum = ', sum);
   });
 
   it( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
     var sum = fibonnaciNums.sumFibs( 4000000, 1, 2 );
     sum.should.equal( 4613732 );
+    console.log('Sum = ', sum);
   });
 });
